7fae3f23b744e3028e64337c18e1fdafb54d0ef4
Kleber <kleberandrade98@gmail.com>
2022-06-21 11:50:26 -0300

    Finalização de cadastro de setor

--- cypress/e2e/setor.cy.js
+++ cypress/e2e/setor.cy.js
@@ -18,7 +18,11 @@ describe('Cadastro de setor', function(){
         cy.get('input[placeholder="Informe sua senha"]').type(login.password)
         cy.get('button[data-testid="button-login"]').click()
 
-        //Acesso ao módulo de setor e cadastro de um novo setor
+        /*
+          Acesso ao módulo de setor e cadastro de um novo setor
+          com herança de módulos de um setor existente.  
+        */
+
 
         //cy.get('a[id="Setores"]').click()
         cy.get(':nth-child(1) > .ant-card-actions > li.ng-star-inserted > span > .ng-star-inserted').click()
@@ -29,14 +33,19 @@ describe('Cadastro de setor', function(){
         let sectorregistration = {
             name: 'Teste Automatizado',
             supervisor: 'Kleber Andrade',
-            
+            description: 'mussum ipsum'            
         }
 
         cy.get('[data-testid="nome"]').type(sectorregistration.name)
         cy.get('#usuario_supervisor_id > .ant-select-selection > .ant-select-selection__rendered > .ant-select-selection__placeholder').type(sectorregistration.supervisor)        
-        cy.wait(3000).get('.ant-select-dropdown-menu > :nth-child(1)').click()
-        //cy.wait(3000).get('#setor > .ant-select-selection > .ant-select-selection__rendered > .ant-select-selection__placeholder').click()
-        
+        cy.wait(1500).get('.ant-select-dropdown-menu > :nth-child(1)').click()
+        cy.get('#setor > .ant-select-selection > .ant-select-selection__rendered > .ant-select-selection__placeholder').click()
+        cy.wait(1500).get('.ant-select-dropdown-menu > :nth-child(2)').click()
+        cy.get('[data-testid="descricao"]').type(sectorregistration.description)
+        cy.get('.footer > :nth-child(2)').click()
+        cy.get('.header > h1').should('have.text', 'Cadastro de setores - resumo')
+        cy.get('.footer > :nth-child(2)').click()
+        cy.get('.mensagem').should('have.text', 'Setor cadastrado com sucesso!')
 
         
         
